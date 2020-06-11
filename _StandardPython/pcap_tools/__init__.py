5729c71057aa7f2cecc1a257161811aced558153
sniky-lyu <junkklv@outlook.com>
2020-06-12 00:11:24 +0800

    pcap

---
+++ _StandardPython/pcap_tools/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author   : Administrator
+# @DateTime : 2020/6/11 22:35
+# @FileName : __init__.py
+# @SoftWare : PyCharm
---
+++ _StandardPython/pcap_tools/loghelper.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author   : Administrator
+# @DateTime : 2020/6/11 23:32
+# @FileName : loghelper.py
+# @SoftWare : PyCharm
+
+
+class LogHandler(object):
+
+    def logger(self):
+        print('logger ******************* ')
+        return 'random'
